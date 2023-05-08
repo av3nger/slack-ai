9771c2498a96a4f1d0377f30453720bd9071b415
Anton Vanyukov <a.vanyukov@vcore.ru>
2023-05-08 21:21:21 +1000

    Set the code to use socket mode

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { App } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
+  await app.start();
+  console.log('⚡️ Bolt app started');
 })();
